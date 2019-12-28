de6b70ee544109d4e9f3b532168787e8e574a9b4
Manav Arora <49856305+manav-1@users.noreply.github.com>
2019-12-28 17:21:27 +0530

    Add files via upload

---
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -0,0 +1,47 @@
+function [X_norm, mu, sigma] = featureNormalize(X)
+%FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. This is often a good preprocessing step to do when
+%   working with learning algorithms.
+
+X_norm = X;
+
+% size(X, 2) = # of columns in matrix X
+% mu and sigma = row vector of size 1xn where n is the # of 
+% rows in X (# of training data)
+mu = zeros(1, size(X, 2));
+sigma = zeros(1, size(X, 2));
+
+% First, for each feature dimension, compute the mean
+% of the feature and subtract it from the dataset,
+% storing the mean value in mu. Next, compute the 
+% standard deviation of each feature and divide
+% each feature by it's standard deviation, storing
+% the standard deviation in sigma. 
+%
+% Note that X is a matrix where each column is a 
+% feature and each row is an example. You need 
+% to perform the normalization separately for 
+% each feature. 
+
+% Parameter X: Any real mxn matrix 
+%      Return: mu = 1xn row vector
+%              sigma = 1xn row vector sigma
+%              X_norm = mxn matrix which has been normalized 
+%                       with mu and sigma
+numberOfColumnsInX_norm = columns(X_norm);
+% works with input matrix X of any size
+for i = 1:numberOfColumnsInX_norm, 
+	meanOfCurrentFeatureInX = mean(X(:, i));
+	mu(:, i) = meanOfCurrentFeatureInX;
+
+	X_norm(:, i) = X_norm(:, i) .- mu(:, i);
+
+	standardDeviationOfCurrentFeatureInX = std(X(:, i));
+	sigma(:, i) = standardDeviationOfCurrentFeatureInX;
+
+	X_norm(:, i) = X_norm(:, i) ./ sigma(:, i);
+end
+
+end
